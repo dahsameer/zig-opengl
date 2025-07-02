b01399f63799e85f126c927e287add3b8a592b28
Sameer Dahal <dahsameer@gmail.com>
2025-07-02 14:48:20 +0600

    triangle showing up

--- src/main.zig
+++ src/main.zig
@@ -13,15 +13,35 @@ fn framebuffer_size_callback(_: ?*c.GLFWwindow, width: c_int, height: c_int) cal
     c.glViewport(0, 0, width, height);
 }
 
-fn processInput(window: ?*c.GLFWwindow) callconv(.C) void {
+fn processInput(window: ?*c.GLFWwindow) void {
     if (c.glfwGetKey(window, c.GLFW_KEY_ESCAPE) == c.GLFW_PRESS) {
         c.glfwSetWindowShouldClose(window, c.GLFW_TRUE);
     }
 }
 
+// shaders
+const vertexShaderSource: [*c]const u8 =
+    \\#version 330 core
+    \\layout (location = 0) in vec3 aPos;
+    \\void main()
+    \\{
+    \\  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    \\}
+;
+const fragmentShaderSource: [*c]const u8 =
+    \\#version 330 core
+    \\out vec4 FragColor;
+    \\void main()
+    \\{
+    \\  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    \\}
+;
+
 pub fn main() !void {
+    // setting a callback for error in glfw
     _ = c.glfwSetErrorCallback(errorCallback);
 
+    // glfw: initialize and configure
     if (c.glfwInit() == c.GLFW_FALSE) {
         std.debug.print("Failed to initialize GLFW\n", .{});
         return error.GLFWInitFailed;
@@ -33,6 +53,7 @@ pub fn main() !void {
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
     c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
 
+    // glfw window creation
     const window = c.glfwCreateWindow(800, 600, "Zig + GLFW + OpenGL", null, null);
     if (window == null) {
         std.debug.print("Failed to create GLFW window\n", .{});
@@ -42,6 +63,7 @@ pub fn main() !void {
     c.glfwMakeContextCurrent(window);
     c.glfwSwapInterval(1);
 
+    // glad: load all opengl function pointers
     std.debug.print("Initializing GLAD...\n", .{});
     if (c.gladLoadGLLoader(@ptrCast(&c.glfwGetProcAddress)) == 0) {
         std.debug.print("Failed to initialize GLAD\n", .{});
@@ -57,7 +79,60 @@ pub fn main() !void {
 
     c.glViewport(0, 0, 800, 600);
     _ = c.glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    const vertices = [_]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 };
+
+    // shader/program status check
+    var success: c_int = undefined;
+    var infoLog: [512:0]u8 = undefined;
+
+    // vertex shader
+    const vertexShader: c_uint = c.glCreateShader(c.GL_VERTEX_SHADER);
+
+    c.glShaderSource(vertexShader, 1, @ptrCast(&vertexShaderSource), null);
+    c.glCompileShader(vertexShader);
+
+    c.glGetShaderiv(vertexShader, c.GL_COMPILE_STATUS, &success);
+    if (success == c.GL_FALSE) {
+        c.glGetShaderInfoLog(vertexShader, 512, null, @ptrCast(&infoLog));
+        std.debug.print("shader error: {s}", .{infoLog});
+    }
+
+    // fragment shader
+    const fragmentShader: c_uint = c.glCreateShader(c.GL_FRAGMENT_SHADER);
+
+    c.glShaderSource(fragmentShader, 1, @ptrCast(&fragmentShaderSource), null);
+    c.glCompileShader(fragmentShader);
+
+    c.glGetShaderiv(fragmentShader, c.GL_COMPILE_STATUS, &success);
+    if (success == c.GL_FALSE) {
+        c.glGetShaderInfoLog(fragmentShader, 512, null, @ptrCast(&infoLog));
+        std.debug.print("shader error: {s}\n", .{infoLog});
+    }
+
+    // link shaders
+    const shaderProgram = c.glCreateProgram();
+
+    c.glAttachShader(shaderProgram, vertexShader);
+    c.glAttachShader(shaderProgram, fragmentShader);
+    c.glLinkProgram(shaderProgram);
+
+    // check for linking error
+    c.glGetProgramiv(shaderProgram, c.GL_LINK_STATUS, &success);
+    if (success == c.GL_FALSE) {
+        c.glGetProgramInfoLog(shaderProgram, 512, null, &infoLog);
+        std.debug.print("program link error: {s}\n", .{infoLog});
+    }
+
+    c.glDeleteShader(vertexShader);
+    c.glDeleteShader(fragmentShader);
+
+    defer c.glDeleteProgram(shaderProgram);
+
+    const vertices = [_]f32{
+        -0.5, -0.5, 0.0, // left
+        0.5, -0.5, 0.0, //right
+        0.0, 0.5, 0.0, //top
+    };
+
     var VAO: c_uint = undefined;
     var VBO: c_uint = undefined;
 
@@ -69,17 +144,29 @@ pub fn main() !void {
         c.glDeleteBuffers(1, &VBO);
     }
 
-    c.glBindBuffer(c.GL_ARRAY_BUFFER, VBO);
+    // bind the vertex array object first, kthen bind and set vertex buffers and then configure vertex attributes
+    c.glBindVertexArray(VAO);
 
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, VBO);
     c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(vertices)), &vertices, c.GL_STATIC_DRAW);
+
+    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), null);
+    c.glEnableVertexAttribArray(0);
+
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, 0);
+    c.glBindVertexArray(0);
 
     while (c.glfwWindowShouldClose(window) == c.GLFW_FALSE) {
         processInput(window);
 
-        c.glClearColor(0.6, 0.3, 0.3, 1.0);
+        c.glClearColor(0.5, 0.3, 0.3, 1.0);
         c.glClear(c.GL_COLOR_BUFFER_BIT);
 
-        c.glfwPollEvents();
+        c.glUseProgram(shaderProgram);
+        c.glBindVertexArray(VAO);
+        c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
+
         c.glfwSwapBuffers(window);
+        c.glfwPollEvents();
     }
 }
